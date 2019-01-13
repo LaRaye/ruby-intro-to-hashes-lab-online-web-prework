a2dcb6e48eaa2f0a2f5e86ba0bbb62c8c24508fd
LaRaye Johnson <lnj2105@gmail.com>
2019-01-13 23:13:31 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -8,12 +8,10 @@ end
 
 def monopoly
 	monopoly = {:railroads => {}}
-	monopoly
 end
 
 def monopoly_with_second_tier
 	monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars => {}}}
-	monopoly
 end
 
 def monopoly_with_third_tier
@@ -29,7 +27,6 @@ def monopoly_with_third_tier
 	     }
 	   }
 	 }
-  monopoly
 end
 
 def monopoly_with_fourth_tier
