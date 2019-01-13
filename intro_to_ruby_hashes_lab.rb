34854d0d8b006f471b3d0a5ba23742be3fdacb67
LaRaye Johnson <lnj2105@gmail.com>
2019-01-13 23:11:23 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -14,10 +14,6 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {:railroads => {:pieces => 4, :names => {}, :rent_in_dollars = {}}}
-	
-	monopoly[:railroads][:pieces] = 4
-	monopoly[:railroads][:names] = {}
-	monopoly[:railroads][:rent_in_dollars] = {}
 	monopoly
 end
 
